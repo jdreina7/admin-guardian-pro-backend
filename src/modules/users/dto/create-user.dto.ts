25a2ef0d5e6d6229c989cffe66e8458b79150ee2
Juan David Reina <judareiro@gmail.com>
2023-09-08 08:22:16 -0500

    AGP-11: Schema y DTO de users listo

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,82 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsMongoId, IsNumber, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNumber()
+  uid: number;
+
+  @IsString()
+  @IsMongoId()
+  identificationTypeId: string;
+
+  @IsEmail()
+  @MinLength(6)
+  email: string;
+
+  @IsString()
+  @MinLength(3)
+  firstName: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  middleName: string;
+
+  @IsString()
+  @MinLength(3)
+  lastName: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  contactPhone: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  address: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  city: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  birthday: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  userImg: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  username: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  @IsMongoId()
+  maritalStatusId: string;
+
+  @IsString()
+  @IsMongoId()
+  ocupationId: string;
+
+  @IsString()
+  @IsMongoId()
+  rolId: string;
+
+  @IsBoolean()
+  @IsOptional()
+  status: boolean;
+
+  @IsString()
+  @MinLength(5)
+  @IsOptional()
+  lastLogin: string;
+}
--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,1 +1,108 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema({
+  // Esto oculta el campo __v y transforma el _id en id en las respuestas
+  toJSON: {
+    virtuals: true,
+    transform: (_doc, ret) => {
+      delete ret.__v;
+      delete ret._id;
+    },
+  },
+  timestamps: true,
+})
+export class User extends mongoose.Document {
+  @Prop({
+    type: Number,
+    unique: true,
+    index: true,
+    required: true,
+  })
+  uid: number;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'IdentificationType',
+    required: true,
+  })
+  identificationTypeId: mongoose.Types.ObjectId;
+
+  @Prop({
+    type: String,
+    unique: true,
+    index: true,
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  firstName: string;
+
+  @Prop()
+  middleName: string;
+
+  @Prop({
+    required: true,
+  })
+  lastName: string;
+
+  @Prop()
+  contactPhone: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  city: string;
+
+  @Prop()
+  birthday: string;
+
+  @Prop()
+  userImg: string;
+
+  @Prop()
+  username: string;
+
+  @Prop({
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'MaritalStatuses',
+    required: true,
+  })
+  maritalStatusId: mongoose.Types.ObjectId;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ocupation',
+    required: true,
+  })
+  ocupationId: mongoose.Types.ObjectId;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Rol',
+    required: true,
+  })
+  rolId: mongoose.Types.ObjectId;
+
+  @Prop({
+    default: true,
+  })
+  status: boolean;
+
+  @Prop()
+  lastLogin: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.index({ uid: 1, type: -1 });
+UserSchema.index({ email: 1, type: -1 });
