4a335044e44d8f9d3c698300747e9e6e93982cbe
Juan David Reina <judareiro@gmail.com>
2023-09-08 20:54:06 -0500

    AGP-20: Schema y DTO del modulo genders listos

--- src/modules/genders/dto/create-gender.dto.ts
+++ src/modules/genders/dto/create-gender.dto.ts
@@ -1,1 +1,17 @@
-export class CreateGenderDto {}
+import { IsString, MinLength, IsIn, IsOptional, IsBoolean } from 'class-validator';
+import { VALID_GENDERS } from 'src/utils/contants';
+
+export class CreateGenderDto {
+  @IsString()
+  @MinLength(1)
+  @IsIn(VALID_GENDERS)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status: boolean;
+}
--- src/modules/genders/schemas/gender.schema.ts
+++ src/modules/genders/schemas/gender.schema.ts
@@ -1,1 +1,32 @@
-export class Gender {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  // Esto oculta el campo __v y transforma el _id en id en las respuestas
+  toJSON: {
+    virtuals: true,
+    transform: (_doc, ret) => {
+      delete ret.__v;
+      delete ret._id;
+    },
+  },
+  timestamps: true,
+})
+export class Gender extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+    required: true,
+  })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({
+    default: true,
+  })
+  status: boolean;
+}
+
+export const GenderSchema = SchemaFactory.createForClass(Gender);
