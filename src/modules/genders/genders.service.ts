3bb729712cbda045357aa911b01045b8c3ed7a30
Juan David Reina <judareiro@gmail.com>
2023-09-09 00:31:11 -0500

    AGP-20: Create gender listo

--- src/modules/genders/genders.service.ts
+++ src/modules/genders/genders.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
+import { Gender } from './schemas/gender.schema';
+import { customCapitalizeFirstLetter, customHandlerCatchException } from 'src/utils/utils';
 
 @Injectable()
 export class GendersService {
-  create(createGenderDto: CreateGenderDto) {
-    return 'This action adds a new gender';
+  constructor(@InjectModel(Gender.name) private readonly genderModel: Model<Gender>) {}
+
+  // Create gender
+  async create(createGenderDto: CreateGenderDto) {
+    createGenderDto.name = await customCapitalizeFirstLetter(createGenderDto.name);
+
+    try {
+      const genderCreated = await this.genderModel.create(createGenderDto);
+
+      return {
+        success: true,
+        data: genderCreated,
+      };
+    } catch (error) {
+      return await customHandlerCatchException(error, createGenderDto);
+    }
   }
 
   findAll() {
