ce95e2b5c99822059560bc5a2b24cbd6d0bcf3d9
Giancarlo <giancarloreina125@gmail.com>
2023-09-22 10:29:23 -0500

    AGP-30: se crearon los test para el controlador del modulo de roles

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -5,13 +5,14 @@ import { Model, isValidObjectId } from 'mongoose';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { Rol } from './schemas/role.schema';
-import { customHandlerCatchException } from 'src/utils/utils';
+import { customHandlerCatchException } from '../../utils/utils';
 import {
   ERR_MSG_DATA_NOT_FOUND,
   ERR_MSG_GENERAL,
-  ERR_MSG_INVALID_ID,
   ERR_MSG_INVALID_PAYLOAD,
-} from 'src/utils/contants';
+  ERR_MSG_INVALID_VALUE,
+} from '../..//utils/contants';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class RolesService {
@@ -34,9 +35,10 @@ export class RolesService {
   }
 
   // Get all roles
-  async findAll() {
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
     try {
-      const data = await this.rolModel.find().sort({ name: 1 });
+      const data = await this.rolModel.find().limit(limit).skip(offset).sort({ name: 1 });
 
       return {
         success: true,
@@ -57,7 +59,7 @@ export class RolesService {
     if (!isValidObjectId(id)) {
       throw new BadRequestException({
         success: false,
-        message: ERR_MSG_INVALID_ID,
+        message: ERR_MSG_INVALID_VALUE,
         invalidValue: `Role ID: ${id}`,
       });
     }
